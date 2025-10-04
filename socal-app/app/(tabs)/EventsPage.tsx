19003ac9477714afe7553f6e6fdf7ccb5a05b02d
arriolajp <arriolajp7@gmail.com>
2025-10-04 17:07:10 -0400

    Added tab layout for different pages in app

---
+++ socal-app/app/(tabs)/EventsPage.tsx
@@ -0,0 +1,8 @@
+import {View, Text} from 'react-native';
+export default function EventsPage() {
+  return(
+  <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+    <Text>Events Page</Text>
+</View>
+  );
+}
--- socal-app/app/(tabs)/_layout.tsx
+++ socal-app/app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import {Ionicons} from '@expo/vector-icons';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
@@ -7,29 +8,14 @@ import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} /> }} />
+      <Tabs.Screen name="events" options={{ title: 'Events', tabBarIcon: ({ color, size }) => <Ionicons name="calendar" color={color} size={size} /> }} />
+      <Tabs.Screen name="business" options={{ title: 'Business', tabBarIcon: ({ color, size }) => <Ionicons name="business" color={color} size={size} /> }} />
+      <Tabs.Screen name="user" options={{ title: 'User', tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} /> }} />
+      <Tabs.Screen name="admin" options={{ title: 'Admin', tabBarIcon: ({ color, size }) => <Ionicons name="shield" color={color} size={size} /> }} />
     </Tabs>
   );
 }
+
